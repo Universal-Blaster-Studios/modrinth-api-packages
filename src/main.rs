8b38b907948a8f3abc5f33e4651a64d1081f5274
LemonHeadOnGit <41242131+LemonHeadOnGit@users.noreply.github.com>
2023-10-25 00:08:27 +0200

    what the hell is a rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use reqwest::blocking::{Client,ClientBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    let http_client = Client::new();
+    let res = http_client.get("https://google.com").send();
+    
+    if res.is_ok() {
+        print!("{:#?}", res.ok().unwrap());
+    }
 }
